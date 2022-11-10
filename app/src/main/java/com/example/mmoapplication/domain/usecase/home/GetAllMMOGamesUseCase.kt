78ae3465abe993660487ee36708dc5ed761d600a
Emerson <emersondobl112@gmail.com>
2022-11-10 20:15:58 -0300

    [MOD] GetAllMMOGamesUseCase: Code refactor

--- app/src/main/java/com/example/mmoapplication/domain/usecase/home/GetAllMMOGamesUseCase.kt
+++ app/src/main/java/com/example/mmoapplication/domain/usecase/home/GetAllMMOGamesUseCase.kt
@@ -3,5 +3,5 @@ package com.example.mmoapplication.domain.usecase.home
 import com.example.mmoapplication.data.model.MMODomain
 
 interface GetAllMMOGamesUseCase {
-    suspend operator fun invoke(): List<MMODomain>
+    suspend fun getGames(): List<MMODomain>
 }
